340ba847b950d4503aacc60eaab19e4c8d4dc616
Seletex <sarr1998@hotmail.com>
2023-06-01 23:32:53 -0500

    Creacion de api

---
+++ TransformateApi/src/main/java/co/edu/uco/transformate/api/TransformateApiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.transformate.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransformateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransformateApiApplication.class, args);
+	}
+
+}
